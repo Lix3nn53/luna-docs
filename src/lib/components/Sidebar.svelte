5b456f5d3c6949cd45f99fa8e231fd733b2f2975
Lix3nn <lixakyildiz@gmail.com>
2025-01-12 16:48:35 +0300

    start code docs

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SidebarDropdownItem, SidebarDropdownWrapper } from 'flowbite-svelte';
 	import { SidebarBrand, Heading, P, A } from 'flowbite-svelte';
 	import logo from '$lib/img/logo128.png';
@@ -19,11 +19,9 @@
 
 	let dropdownItemClass = itemClass + " ml-4 ps-4";
 	let dropdownActiveClass = itemClass + " ml-4 ps-4 " + activeClass;
-
-	$: activeUrl = $page.url.pathname;
 </script>
   
-  <Sidebar class="min-h-full" {activeUrl}>
+  <Sidebar class="min-h-full" activeUrl={page.url.pathname}>
 	<SidebarWrapper class="min-h-full" divClass={"overflow-y-auto py-4 px-3 bg-stone-50 dark:bg-stone-800"}>
 	  	<SidebarGroup>
 			<SidebarBrand {site} />
@@ -51,63 +49,63 @@
 			<SidebarItem class={itemClass} activeClass={activeClass} nonActiveClass={nonActiveClass} label="Gallery" href="/luna/gallery" />
 			<SidebarItem class={itemClass} activeClass={activeClass} nonActiveClass={nonActiveClass} label="Why UI Toolkit?" href="/luna/whyuitk" />
 			<SidebarItem class={itemClass} activeClass={activeClass} nonActiveClass={nonActiveClass} label="Quick Start" href="/luna/quick" />
-			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Samples" isOpen={activeUrl.includes("/luna/samples")}>
+			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Samples" isOpen={page.url.pathname.includes("/luna/samples")}>
 				<svelte:fragment slot="icon">
 					<i class="fa-solid fa-box"></i>
 				</svelte:fragment>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Components" href="/luna/samples/components" active={activeUrl.includes("/luna/samples/components")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Game" href="/luna/samples/game" active={activeUrl.includes("/luna/samples/game")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Game Addressables" href="/luna/samples/addressables" active={activeUrl.includes("/luna/samples/addressables")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Localization" href="/luna/samples/localization" active={activeUrl.includes("/luna/samples/localization")}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Components" href="/luna/samples/components" active={page.url.pathname === "/luna/samples/components"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Game" href="/luna/samples/game" active={page.url.pathname === "/luna/samples/game"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Game Addressables" href="/luna/samples/addressables" active={page.url.pathname === "/luna/samples/addressables"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Localization" href="/luna/samples/localization" active={page.url.pathname === "/luna/samples/localization"}/>
 			</SidebarDropdownWrapper>
-			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Framework" isOpen={activeUrl.includes("/luna/framework")}>
+			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Framework" isOpen={page.url.pathname.includes("/luna/framework")}>
 				<svelte:fragment slot="icon">
 					<i class="fa-solid fa-code"></i>
 				</svelte:fragment>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={activeUrl.includes("/luna/framework/uiview")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIViewComponent" href="/luna/framework/uiviewcomponent" active={activeUrl.includes("/luna/framework/uiviewcomponent")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="FadeUIElement" href="/luna/framework/FadeUIElement" active={activeUrl.includes("/luna/framework/FadeUIElement")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIActions" href="/luna/framework/UIActions" active={activeUrl.includes("/luna/framework/UIActions")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Example: Multi Page UI" href="/luna/framework/example" active={activeUrl.includes("/luna/framework/example")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIElementManager" href="/luna/framework/UIElementManager" active={activeUrl.includes("/luna/framework/UIElementManager")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UI Navigation" href="/luna/framework/UINavigation" active={activeUrl.includes("/luna/framework/UINavigation")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Localization" href="/luna/framework/localization" active={activeUrl.includes("/luna/framework/localization")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Drag & Drop" href="/luna/framework/dragdrop" active={activeUrl.includes("/luna/framework/dragdrop")}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={page.url.pathname === "/luna/framework/uiview"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIViewComponent" href="/luna/framework/uiviewcomponent" active={page.url.pathname === "/luna/framework/uiviewcomponent"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="FadeUIElement" href="/luna/framework/FadeUIElement" active={page.url.pathname === "/luna/framework/FadeUIElement"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIActions" href="/luna/framework/UIActions" active={page.url.pathname === "/luna/framework/UIActions"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Example: Multi Page UI" href="/luna/framework/example01" active={page.url.pathname === "/luna/framework/example01"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIElementManager" href="/luna/framework/UIElementManager" active={page.url.pathname === "/luna/framework/UIElementManager"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UI Navigation" href="/luna/framework/UINavigation" active={page.url.pathname === "/luna/framework/UINavigation"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Localization" href="/luna/framework/localization" active={page.url.pathname === "/luna/framework/localization"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Drag & Drop" href="/luna/framework/dragdrop" active={page.url.pathname === "/luna/framework/dragdrop"}/>
 			</SidebarDropdownWrapper>
-			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Components" isOpen={activeUrl.includes("/luna/components")}>
+			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Components" isOpen={page.url.pathname.includes("/luna/components")}>
 				<svelte:fragment slot="icon">
 					<i class="fa-solid fa-layer-group"></i>
 				</svelte:fragment>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={activeUrl.includes("/luna/framework/uiview")}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={page.url.pathname === "/luna/framework/uiview"}/>
 			</SidebarDropdownWrapper>
-			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Views" isOpen={activeUrl.includes("/luna/views")}>
+			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Views" isOpen={page.url.pathname.includes("/luna/views")}>
 				<svelte:fragment slot="icon">
 					<i class="fa-solid fa-window-restore"></i>
 				</svelte:fragment>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={activeUrl.includes("/luna/framework/uiview")}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={page.url.pathname === "/luna/framework/uiview"}/>
 			</SidebarDropdownWrapper>
-			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Styling" isOpen={activeUrl.includes("/luna/styling")}>
+			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Styling" isOpen={page.url.pathname.includes("/luna/styling")}>
 				<svelte:fragment slot="icon">
 					<i class="fa-solid fa-palette"></i>
 				</svelte:fragment>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={activeUrl.includes("/luna/framework/uiview")}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="UIView" href="/luna/framework/uiview" active={page.url.pathname === "/luna/framework/uiview"}/>
 			</SidebarDropdownWrapper>
 		</SidebarGroup>
 		<SidebarGroup border {borderClass}>
-			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Core Library" isOpen={activeUrl.includes("/core")}>
+			<SidebarDropdownWrapper btnClass={dropdownWrapperClass} label="Core Library" isOpen={page.url.pathname.includes("/core")}>
 				<svelte:fragment slot="icon">
 					<i class="fa-solid fa-gears"></i>
 				</svelte:fragment>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Service Locator" href="/core/servicelocator" active={activeUrl.includes("/core/servicelocator")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Scene Management Addressable" href="/core/scenemanagementaddressable" active={activeUrl.includes("/core/scenemanagementaddressable")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="KeyValue Database" href="/core/KeyValueDatabase" active={activeUrl.includes("/core/KeyValueDatabase")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Prefab Loader" href="/core/prefabloader" active={activeUrl.includes("/core/prefabloader")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Addressable Asset Manager" href="/core/addressableassetmanager" active={activeUrl.includes("/core/addressableassetmanager")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Pool" href="/core/pool" active={activeUrl.includes("/core/pool")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Fadeable" href="/core/fadeable" active={activeUrl.includes("/core/fadeable")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Input Escape Manager" href="/core/inputescapemanager" active={activeUrl.includes("/core/inputescapemanager")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Game Save Manager" href="/core/gamesavemanager" active={activeUrl.includes("/core/gamesavemanager")}/>
-				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Behaviour Tree" href="/core/behaviourtree" active={activeUrl.includes("/core/behaviourtree")}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Service Locator" href="/core/servicelocator" active={page.url.pathname === "/core/servicelocator"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Scene Management Addressable" href="/core/scenemanagementaddressable" active={page.url.pathname === "/core/scenemanagementaddressable"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="KeyValue Database" href="/core/KeyValueDatabase" active={page.url.pathname === "/core/KeyValueDatabase"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Prefab Loader" href="/core/prefabloader" active={page.url.pathname === "/core/prefabloader"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Addressable Asset Manager" href="/core/addressableassetmanager" active={page.url.pathname === "/core/addressableassetmanager"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Pool" href="/core/pool" active={page.url.pathname === "/core/pool"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Fadeable" href="/core/fadeable" active={page.url.pathname === "/core/fadeable"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Input Escape Manager" href="/core/inputescapemanager" active={page.url.pathname === "/core/inputescapemanager"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Game Save Manager" href="/core/gamesavemanager" active={page.url.pathname === "/core/gamesavemanager"}/>
+				<SidebarDropdownItem aClass={dropdownItemClass} activeClass={dropdownActiveClass} label="Behaviour Tree" href="/core/behaviourtree" active={page.url.pathname === "/core/behaviourtree"}/>
 			</SidebarDropdownWrapper>
 		</SidebarGroup>
 	</SidebarWrapper>
