a93d4a6b24821f80f645dd7b9b39609334f192be
Lix3nn <lixakyildiz@gmail.com>
2025-01-14 06:15:38 +0300

    border and cursor

--- src/routes/luna/styling/border/+page.svelte
+++ src/routes/luna/styling/border/+page.svelte
@@ -1,7 +1,18 @@
 <script>
-	import { Heading, P, A, Mark, Secondary, GradientButton, Hr, Li, List } from 'flowbite-svelte';
-	import confirmationpopup_ex from '$lib/img/docs/components/confirmationpopup_ex.png';
-	import confirmationpopup_ins from '$lib/img/docs/components/confirmationpopup_ins.jpg';
+	import {
+		Heading,
+		P,
+		A,
+		Mark,
+		Secondary,
+		GradientButton,
+		Hr,
+		Li,
+		List,
+		Button
+	} from 'flowbite-svelte';
+	import setup_theme from '$lib/img/docs/styling/setup_theme.jpg';
+	import setup_panel_settings from '$lib/img/docs/styling/setup_panel_settings.jpg';
 
 	import { CodeBlock } from 'svhighlight';
 	import 'highlight.js/styles/base16/papercolor-dark.css';
@@ -15,7 +26,7 @@
 		TableHeadCell
 	} from 'flowbite-svelte';
 
-	let tdheadClass = 'px-6 py-4 whitespace-normal font-medium bg-stone-800 text-stone-50';
+	let tdheadClass = 'px-6 py-2 whitespace-normal font-medium bg-stone-800 text-stone-50';
 	let tdrowClass = 'border-stone-500';
 	let tdClass = 'px-6 py-2 whitespace-normal font-medium bg-stone-700';
 	let tdClasss = 'px-6 py-2 whitespace-normal font-medium bg-stone-600 min-w-48';
@@ -25,5 +36,120 @@
 	>Styling/ Border</Heading
 >
 <div class="p-4">
-	<P>TODO</P>
+	<Heading tag="h2" class="my-4" customSize="text-xl font-bold md:text-2xl lg:text-3xl"
+		>Example</Heading
+	>
+	<CodeBlock
+		language="html"
+		code={`
+<!-- Set border width, radius and color -->
+<engine:VisualElement class="border-8 rounded-lg border-red-400"/>`}
+		showHeader={false}
+		showLineNumbers={false}
+		background="bg-zinc-900"
+		headerClasses="bg-zinc-800 text-white/80 text-xs font-bold"
+	/>
+	<Heading tag="h2" class="my-4" customSize="text-xl font-bold md:text-2xl lg:text-3xl"
+		>Border Width</Heading
+	>
+	<P><strong class="font-extrabold">Format:</strong> border-{'{'}value{'}'}</P>
+	<Table class="rounded">
+		<TableHead>
+			<TableHeadCell class={tdheadClass}>Class</TableHeadCell>
+			<TableHeadCell class={tdheadClass}>Properties</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>border-0</TableBodyCell>
+				<TableBodyCell {tdClass}>border-width: 0</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>border-1</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}>border-width: 1px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>border-2</TableBodyCell>
+				<TableBodyCell {tdClass}>border-width: 2px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>border-4</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}>border-width: 4px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>border-8</TableBodyCell>
+				<TableBodyCell {tdClass}>border-width: 8px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>border-12</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}>border-width: 12px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>border-16</TableBodyCell>
+				<TableBodyCell {tdClass}>border-width: 16px</TableBodyCell>
+			</TableBodyRow>
+		</TableBody>
+	</Table>
+	<Heading tag="h2" class="my-4" customSize="text-xl font-bold md:text-2xl lg:text-3xl"
+		>Border Radius</Heading
+	>
+	<P><strong class="font-extrabold">Format:</strong> rounded-{'{'}value{'}'}</P>
+	<Table class="rounded">
+		<TableHead>
+			<TableHeadCell class={tdheadClass}>Class</TableHeadCell>
+			<TableHeadCell class={tdheadClass}>Properties</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>rounded-none</TableBodyCell>
+				<TableBodyCell {tdClass}>border-radius: 0</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>rounded-sm</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}>border-radius: 2px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>rounded</TableBodyCell>
+				<TableBodyCell {tdClass}>border-radius: 4px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>rounded-md</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}>border-radius: 6px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>rounded-lg</TableBodyCell>
+				<TableBodyCell {tdClass}>border-radius: 8px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>rounded-xl</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}>border-radius: 12px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>rounded-2xl</TableBodyCell>
+				<TableBodyCell {tdClass}>border-radius: 16px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>rounded-3xl</TableBodyCell>
+				<TableBodyCell {tdClass}>border-radius: 24px</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>rounded-full</TableBodyCell>
+				<TableBodyCell {tdClass}>border-radius: 9999px</TableBodyCell>
+			</TableBodyRow>
+		</TableBody>
+	</Table>
+	<Heading tag="h2" class="my-4" customSize="text-xl font-bold md:text-2xl lg:text-3xl"
+		>Border Color</Heading
+	>
+	<P><strong class="font-extrabold">Format:</strong> border-{'{'}color{'}'}-{'{'}tone{'}'}</P>
+	<CodeBlock
+		language="html"
+		code={`
+<engine:VisualElement class="border-purple-700" />
+<engine:VisualElement class="border-red-500" />
+<engine:VisualElement class="border-red-500-half" />`}
+		showHeader={false}
+		showLineNumbers={false}
+		background="bg-zinc-900"
+		headerClasses="bg-zinc-800 text-white/80 text-xs font-bold"
+	/>
 </div>
--- src/routes/luna/styling/cursor/+page.svelte
+++ src/routes/luna/styling/cursor/+page.svelte
@@ -1,7 +1,33 @@
 <script>
-	import { Heading, P, A, Mark, Secondary, GradientButton, Hr, Li, List } from 'flowbite-svelte';
-	import confirmationpopup_ex from '$lib/img/docs/components/confirmationpopup_ex.png';
-	import confirmationpopup_ins from '$lib/img/docs/components/confirmationpopup_ins.jpg';
+	import {
+		Heading,
+		P,
+		A,
+		Mark,
+		Secondary,
+		GradientButton,
+		Hr,
+		Li,
+		List,
+		Button
+	} from 'flowbite-svelte';
+	import setup_theme from '$lib/img/docs/styling/setup_theme.jpg';
+	import setup_panel_settings from '$lib/img/docs/styling/setup_panel_settings.jpg';
+
+	import bracket_a_vertical from '$lib/img/docs/styling/cursor/bracket_a_vertical.png';
+	import cursor_busy from '$lib/img/docs/styling/cursor/cursor_busy.png';
+	import cursor_copy from '$lib/img/docs/styling/cursor/cursor_copy.png';
+	import cursor_disabled from '$lib/img/docs/styling/cursor/cursor_disabled.png';
+	import cursor_help from '$lib/img/docs/styling/cursor/cursor_help.png';
+	import cursor_menu from '$lib/img/docs/styling/cursor/cursor_menu.png';
+	import hand_small_closed from '$lib/img/docs/styling/cursor/hand_small_closed.png';
+	import hand_small_open from '$lib/img/docs/styling/cursor/hand_small_open.png';
+	import hand_small_point from '$lib/img/docs/styling/cursor/hand_small_point.png';
+	import line_cross from '$lib/img/docs/styling/cursor/line_cross.png';
+	import pointer_a from '$lib/img/docs/styling/cursor/pointer_a.png';
+	import resize_a_cross from '$lib/img/docs/styling/cursor/resize_a_cross.png';
+	import zoom_in from '$lib/img/docs/styling/cursor/zoom_in.png';
+	import zoom_out from '$lib/img/docs/styling/cursor/zoom_out.png';
 
 	import { CodeBlock } from 'svhighlight';
 	import 'highlight.js/styles/base16/papercolor-dark.css';
@@ -15,7 +41,7 @@
 		TableHeadCell
 	} from 'flowbite-svelte';
 
-	let tdheadClass = 'px-6 py-4 whitespace-normal font-medium bg-stone-800 text-stone-50';
+	let tdheadClass = 'px-6 py-2 whitespace-normal font-medium bg-stone-800 text-stone-50';
 	let tdrowClass = 'border-stone-500';
 	let tdClass = 'px-6 py-2 whitespace-normal font-medium bg-stone-700';
 	let tdClasss = 'px-6 py-2 whitespace-normal font-medium bg-stone-600 min-w-48';
@@ -25,5 +51,139 @@
 	>Styling/ Cursor</Heading
 >
 <div class="p-4">
-	<P>TODO</P>
+	Change cursor icon when hovered.
+	<Heading tag="h2" class="my-4" customSize="text-xl font-bold md:text-2xl lg:text-3xl"
+		>USS Classes</Heading
+	>
+	<P><strong class="font-extrabold">Format:</strong> cursor-{'{'}value{'}'}</P>
+	<CodeBlock
+		language="html"
+		code={`
+<!-- Change cursor icon on hover -->
+<engine:VisualElement class="cursor-move"/>`}
+		showHeader={false}
+		showLineNumbers={false}
+		background="bg-zinc-900"
+		headerClasses="bg-zinc-800 text-white/80 text-xs font-bold"
+	/>
+	<br />
+	<Table class="rounded">
+		<TableHead>
+			<TableHeadCell class={tdheadClass}>Class</TableHeadCell>
+			<TableHeadCell class={tdheadClass}>Icon</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-default</TableBodyCell>
+				<TableBodyCell {tdClass}><img alt="pointer_a" src={pointer_a} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-pointer</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="hand_small_point" src={hand_small_point} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-wait</TableBodyCell>
+				<TableBodyCell {tdClass}
+					><img alt="cursor_busy" src={cursor_busy} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-text</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="bracket_a_vertical" src={bracket_a_vertical} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-move</TableBodyCell>
+				<TableBodyCell {tdClass}
+					><img alt="resize_a_cross" src={resize_a_cross} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-help</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="cursor_help" src={cursor_help} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-not-allowed</TableBodyCell>
+				<TableBodyCell {tdClass}
+					><img alt="cursor_disabled" src={cursor_disabled} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-context-menu</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="cursor_menu" src={cursor_menu} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-cell</TableBodyCell>
+				<TableBodyCell {tdClass}
+					><img alt="line_cross" src={line_cross} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-copy</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="cursor_copy" src={cursor_copy} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-grab</TableBodyCell>
+				<TableBodyCell {tdClass}
+					><img alt="hand_small_open" src={hand_small_open} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-grabbing</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="hand_small_closed" src={hand_small_closed} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell {tdClass}>cursor-zoom-in</TableBodyCell>
+				<TableBodyCell {tdClass}><img alt="zoom_in" src={zoom_in} class="w-12" /></TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow color="custom" class={tdrowClass}>
+				<TableBodyCell tdClass={tdClasss}>cursor-zoom-out</TableBodyCell>
+				<TableBodyCell tdClass={tdClasss}
+					><img alt="zoom_out" src={zoom_out} class="w-12" /></TableBodyCell
+				>
+			</TableBodyRow>
+		</TableBody>
+	</Table>
+	<Heading tag="h2" class="my-4" customSize="text-xl font-bold md:text-2xl lg:text-3xl"
+		>USS Variables</Heading
+	>
+	<CodeBlock
+		language="css"
+		code={`
+--cursor-default
+--cursor-pointer
+--cursor-wait
+--cursor-text
+--cursor-move
+--cursor-help
+--cursor-not-allowed
+--cursor-context-menu
+--cursor-cell
+--cursor-copy
+--cursor-grab
+--cursor-grabbing
+--cursor-zoom-in
+--cursor-zoom-out
+
+/* Usage */
+.example {
+    cursor: var(--cursor-pointer);
+}`}
+		showHeader={false}
+		showLineNumbers={false}
+		background="bg-zinc-900"
+		headerClasses="bg-zinc-800 text-white/80 text-xs font-bold"
+	/>
 </div>
