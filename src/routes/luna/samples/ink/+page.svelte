2f76fcbbf8990db17bdc92c27f839b0db091f733
Lix3nn <lixakyildiz@gmail.com>
2025-02-08 17:51:20 +0300

    ink

---
+++ src/routes/luna/samples/ink/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Heading, P, A, Mark, Secondary, GradientButton, Hr } from 'flowbite-svelte';
+	import CreateLocalizationSettings from '$lib/img/docs/quick/CreateLocalizationSettings.png';
+	import window from '$lib/img/docs/quick/window.png';
+</script>
+
+<Heading tag="h1" customSize="text-2xl font-extrabold md:text-3xl lg:text-4xl"
+	>Samples/ Ink</Heading
+>
+<div class="p-4">
+	<P class="mb-2">
+		A sample demonstrating how to integrate the Ink narrative scripting language with the VisualNovel component. With support for choices and branching.
+	</P>
+</div>
